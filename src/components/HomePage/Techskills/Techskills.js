321c6681443d9e6a1bc56ec5ae2d15c5f95210a5
Akash Chhetri <akash.chhetri1@gmail.com>
2020-10-21 16:48:44 +1100

    technical skills added

--- src/components/HomePage/Techskills/Techskills.js
+++ src/components/HomePage/Techskills/Techskills.js
@@ -1,36 +1,96 @@
 import React from 'react';
+import { Row, Col, Container, Card, Button } from 'react-bootstrap';
 
 import techSkillsStyle from './techSkills.module.css';
-import { Row, Col, Card, Container } from 'react-bootstrap';
 
 const Techskills = () => {
 	return (
 		<div className={techSkillsStyle.Container}>
 			<h4 className={techSkillsStyle.Title}>Technical Skills</h4>
-			<Container>
-				<Row>
+			<Container className="mt-5">
+				<Row noGutters>
 					<Col sm={12} md={4} className={techSkillsStyle.CardFlip}>
 						<div className={techSkillsStyle.CardFront}>
-							<Card>Front end</Card>
+							<Card bg="danger" text="white">
+								<Card.Header>Frontend</Card.Header>
+								<Card.Body>
+									<Card.Title>
+										HTML, CSS, JS, ReactJS
+									</Card.Title>
+									<Card.Text>
+										Experienced with front end skills, can
+										develop static and dynamic websites.
+										Please check my projects.
+									</Card.Text>
+								</Card.Body>
+							</Card>
 						</div>
 						<div className={techSkillsStyle.CardBack}>
-							<Card>Front ka back</Card>
+							<Card bg="warning" text="white">
+								<Card.Header>Frontend</Card.Header>
+								<Card.Body>
+									<Card.Title>
+										HTML, CSS, JS, ReactJS
+									</Card.Title>
+									<Card.Text>
+										Experienced with front end skills, can
+										develop static and dynamic websites.
+										Please check my projects.
+									</Card.Text>
+								</Card.Body>
+							</Card>
 						</div>
 					</Col>
 					<Col sm={12} md={4} className={techSkillsStyle.CardFlip}>
 						<div className={techSkillsStyle.CardFront}>
-							<Card>Back end</Card>
+							<Card bg="warning" text="white">
+								<Card.Header>Backend</Card.Header>
+								<Card.Body>
+									<Card.Title>NodeJS, Python</Card.Title>
+									<Card.Text>
+										Experienced with back end skills
+									</Card.Text>
+								</Card.Body>
+							</Card>
 						</div>
 						<div className={techSkillsStyle.CardBack}>
-							<Card>Back ka back</Card>
+							<Card bg="success" text="white">
+								<Card.Header>Backend</Card.Header>
+								<Card.Body>
+									<Card.Title>NodeJS, Python</Card.Title>
+									<Card.Text>
+										Experienced with back end skills
+									</Card.Text>
+								</Card.Body>
+							</Card>
 						</div>
 					</Col>
 					<Col sm={12} md={4} className={techSkillsStyle.CardFlip}>
 						<div className={techSkillsStyle.CardFront}>
-							<Card>AWS</Card>
+							<Card bg="success" text="white">
+								<Card.Header>Cloud and Database</Card.Header>
+								<Card.Body>
+									<Card.Title>
+										AWS, SQL, MongoDB, Mongoose
+									</Card.Title>
+									<Card.Text>
+										Experienced with Database
+									</Card.Text>
+								</Card.Body>
+							</Card>
 						</div>
 						<div className={techSkillsStyle.CardBack}>
-							<Card>AWS ka back</Card>
+							<Card bg="danger" text="white">
+								<Card.Header>Cloud and Database</Card.Header>
+								<Card.Body>
+									<Card.Title>
+										AWS, SQL, MongoDB, Mongoose
+									</Card.Title>
+									<Card.Text>
+										Experienced with Database
+									</Card.Text>
+								</Card.Body>
+							</Card>
 						</div>
 					</Col>
 				</Row>
