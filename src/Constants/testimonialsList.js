6f03ebcb3ab4779a315c3323408f9f53cc1425df
Akash Chhetri <akash.chhetri1@gmail.com>
2020-10-27 21:29:28 +1100

    carousel done

---
+++ src/Constants/testimonialsList.js
@@ -0,0 +1,23 @@
+export default [
+	{
+		name: 'Mayur XXX',
+		position: 'Subha sandhaya',
+		imageUrl:
+			'https://cdn.dnaindia.com/sites/default/files/styles/full/public/2017/12/01/629180-srk-120217.jpg',
+		comment: 'He is a good student',
+	},
+	{
+		name: 'Mayur XXX',
+		position: 'Shavans',
+		imageUrl:
+			'https://cdn.dnaindia.com/sites/default/files/styles/full/public/2017/12/01/629180-srk-120217.jpg',
+		comment: 'He is a good student',
+	},
+	{
+		name: 'Mayur XXX',
+		position: 'Tandoori Temptation',
+		imageUrl:
+			'https://www.thestatesman.com/wp-content/uploads/2020/04/BeFunky-collage-7-1.jpg',
+		comment: 'He is a good student',
+	},
+];
--- src/components/HomePage/Testimonials/Testimonials.js
+++ src/components/HomePage/Testimonials/Testimonials.js
@@ -1,15 +1,99 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import testimonialsStyle from './testimonials.module.css';
+import testimonialsList from '../../../Constants/testimonialsList';
 
 const Testimonials = () => {
+	const [index, setIndex] = useState(0);
+
+	const carouselHandler = (direction) => {
+		const length = testimonialsList.length;
+
+		switch (direction) {
+			case 'left':
+				index === 0
+					? setIndex(() => length - 1)
+					: setIndex((prevIndex) => prevIndex - 1);
+				break;
+			case 'right':
+				index === length - 1
+					? setIndex(() => 0)
+					: setIndex((prevIndex) => prevIndex + 1);
+				break;
+			default:
+				return;
+		}
+	};
+
+	useEffect(() => {
+		console.log(index, 'index changed');
+	});
+
+	const Carousel = ({ number }) => {
+		const { name, position, imageUrl, comment } = testimonialsList[number];
+		return (
+			<Row className={testimonialsStyle.CarouselContainer}>
+				<Col
+					xs={12}
+					md={4}
+					className={testimonialsStyle.ImageContainer}
+				>
+					<Image
+						src={imageUrl}
+						roundedCircle
+						className={testimonialsStyle.Image}
+					/>
+					<div className={testimonialsStyle.TitleContainer}>
+						<h5>{name}</h5>
+						<p>{position}</p>
+					</div>
+				</Col>
+				<Col
+					xs={12}
+					md={8}
+					className={testimonialsStyle.QuoteContainer}
+				>
+					<FontAwesomeIcon
+						icon={['fas', 'quote-left']}
+						size="1x"
+						className={testimonialsStyle.FaLeftQuote}
+					/>
+					<p>{comment}</p>
+					<FontAwesomeIcon
+						icon={['fas', 'quote-right']}
+						size="1x"
+						className={testimonialsStyle.FaRightQuote}
+					/>
+				</Col>
+			</Row>
+		);
+	};
+
 	return (
 		<div className={testimonialsStyle.Container}>
-			<h4 className={testimonialsStyle.Title}>Technical Skills</h4>
-			<Container className="mt-5" data-aos="fade-up">
-				<Row noGutters>
-					<Col>Hi</Col>
-				</Row>
+			<h4 className={testimonialsStyle.Title}>Testimonials</h4>
+			<Container
+				data-aos="fade-up"
+				className="d-flex justify-content-center"
+			>
+				<div className={testimonialsStyle.FaContainer}>
+					<FontAwesomeIcon
+						icon={['fas', 'angle-left']}
+						size="3x"
+						className={testimonialsStyle.FaLeft}
+						onClick={() => carouselHandler('left')}
+					/>
+					<FontAwesomeIcon
+						icon={['fas', 'angle-right']}
+						size="3x"
+						className={testimonialsStyle.FaRight}
+						onClick={() => carouselHandler('right')}
+					/>
+				</div>
+
+				<Carousel number={index} />
 			</Container>
 		</div>
 	);
