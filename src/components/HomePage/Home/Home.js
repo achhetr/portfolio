43ce505c0fcb0cb7fe4a610479d466a6c6dfeadc
Akash Chhetri <akash.chhetri1@gmail.com>
2020-11-03 14:48:46 +1100

    home screen bug fixed

--- src/components/HomePage/Home/Home.js
+++ src/components/HomePage/Home/Home.js
@@ -6,49 +6,49 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import homeStyle from './home.module.css';
 
 const Home = () => (
-	<div className={homeStyle.HomeContainer}>
-		<div className={homeStyle.Home}>
-			<h2 className={homeStyle.Title}>Akash Chhetri</h2>
-			<h4 className={homeStyle.Profile}>
-				I am a Junior Full Stack Developer
-			</h4>
-			<Link to="/projects" className={homeStyle.ProjectBtn}>
-				<Button variant="outline-primary">Check my projects</Button>{' '}
-			</Link>
-			<div>
-				<a
-					href="https://github.com/achhetr"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<FontAwesomeIcon
-						icon={['fab', 'github']}
-						className={`${homeStyle.FaItem} ${homeStyle.FaItemGit}`}
-						size="2x"
-					/>
-				</a>
-				<a
-					href="https://www.linkedin.com/in/akashchhetri/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<FontAwesomeIcon
-						icon={['fab', 'linkedin-in']}
-						className={`${homeStyle.FaItem} ${homeStyle.FaItemLkd}`}
-						size="2x"
-					/>
-				</a>
-			</div>
-			<p>Scroll Down</p>
-			<div className={homeStyle.MoveScroll}>
+	// <div className={homeStyle.HomeContainer}>
+	<div className={homeStyle.Home}>
+		<h2 className={homeStyle.Title}>Akash Chhetri</h2>
+		<h4 className={homeStyle.Profile}>
+			I am a Junior Full Stack Developer
+		</h4>
+		<Link to="/projects" className={homeStyle.ProjectBtn}>
+			<Button variant="outline-primary">Check my projects</Button>{' '}
+		</Link>
+		<div>
+			<a
+				href="https://github.com/achhetr"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
 				<FontAwesomeIcon
-					icon={['fas', 'arrow-circle-down']}
-					className={`${homeStyle.FaItem} ${homeStyle.FaItemDwn}`}
+					icon={['fab', 'github']}
+					className={`${homeStyle.FaItem} ${homeStyle.FaItemGit}`}
 					size="2x"
 				/>
-			</div>
+			</a>
+			<a
+				href="https://www.linkedin.com/in/akashchhetri/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<FontAwesomeIcon
+					icon={['fab', 'linkedin-in']}
+					className={`${homeStyle.FaItem} ${homeStyle.FaItemLkd}`}
+					size="2x"
+				/>
+			</a>
+		</div>
+		<p>Scroll Down</p>
+		<div className={homeStyle.MoveScroll}>
+			<FontAwesomeIcon
+				icon={['fas', 'arrow-circle-down']}
+				className={`${homeStyle.FaItem} ${homeStyle.FaItemDwn}`}
+				size="2x"
+			/>
 		</div>
 	</div>
+	// </div>
 );
 
 export default Home;
